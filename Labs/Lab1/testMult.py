ffad2d9bba33d9a7993089fd2ecf2d5af725a3b1
Ella Finstuen <ellafinstuen@econ2-192-93-dhcp.int.colorado.edu>
2024-09-05 12:22:42 -0600

    First comment

--- Labs/Lab1/testMult.py
+++ Labs/Lab1/testMult.py
@@ -31,7 +31,7 @@ def matrixMult(a,b):
     c[0,0] = a[0,0] * b[0,0] + a[0,1] * b[1,0]
     c[0,1] = a[0,0] * b[0,1] + a[0,1] * b[1,1]
     c[1,0] = a[1,0] * b[0,0] + a[1,1] * b[1,0] 
-    c[1,1] = a[1,0] * b[0,1] + a[1,1] * b[1,1]
+    c[1,1] = a[1,0] * b[0,1] + a[1,1] * b[1,1] #additions and multiplications to create entries for c
 
     return c
 
